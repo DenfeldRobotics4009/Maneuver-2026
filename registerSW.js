5e77a96e30253770c8ac36e006c85bbdb2b69394
IanOlds <iolds668@gmail.com>
2026-02-04 10:44:21 -0600

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Maneuver-2026/sw.js', { scope: '/Maneuver-2026/' })})}
